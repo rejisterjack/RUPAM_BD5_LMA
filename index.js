37698bb467fc289138f02821391b0f4457a83249
Rupam Das <rejisterjack@gmail.com>
2025-01-27 19:41:10 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express")
+const { resolve } = require("path")
 
-const app = express();
-const port = 3010;
+const app = express()
+const port = 3000
 
-app.use(express.static('static'));
+app.use(express.static("static"))
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"))
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
